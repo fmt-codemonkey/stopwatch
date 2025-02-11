e121bbbefe6553e02f7aa6c800d2d2c60b58dd65
fmt-codemonkey <fmt.codemonkey@gmail.com>
2025-02-11 01:18:28 -0500

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.sagarassignment1"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.sagarassignment1"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.play.services.wearable)
+}
